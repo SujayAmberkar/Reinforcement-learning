9ae24f0eeef8680ef849c4856bf3ae1891335578
SujayAmberkar <sujayamberkar@gmail.com>
2023-12-07 23:33:01 +0530

    gymnasium environment setup

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import gymnasium as gym
+env = gym.make("LunarLander-v2", render_mode="human")
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+   action = env.action_space.sample()  # this is where you would insert your policy
+   observation, reward, terminated, truncated, info = env.step(action)
 
-from stable_baselines3 import A2C
+   if terminated or truncated:
+      observation, info = env.reset()
 
-env = gym.make("CartPole-v1", render_mode="rgb_array")
-
-model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10_000)
-
-vec_env = model.get_env()
-obs = vec_env.reset()
-for i in range(1000):
-    action, _state = model.predict(obs, deterministic=True)
-    obs, reward, done, info = vec_env.step(action)
-    vec_env.render("human")
-    # VecEnv resets automatically
-    # if done:
-    #   obs = vec_env.reset()
+env.close()
