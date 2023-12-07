2b1ba3ce1a2c2ce40d84618b0f335a360369b2a5
SujayAmberkar <sujayamberkar@gmail.com>
2023-12-08 00:16:01 +0530

    trained lunar lander

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import gymnasium as gym
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_vec_env
 
-env = gym.make("CartPole-v1", render_mode="human")
+env = gym.make("LunarLander-v2", render_mode="human")
 model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=25000)
-model.save("a2c_cartpole")
+# model.learn(total_timesteps=250000)
+# model.save("./a2c_cartpole")
 
-del model # remove to demonstrate saving and loading
+# del model # remove to demonstrate saving and loading
 
-model = A2C.load("a2c_cartpole")
+model = A2C.load("./a2c_cartpole")
 
 observation, info = env.reset(seed=42)
 
-for _ in range(1000):
+for _ in range(10000):
    action,_states = model.predict(observation)  # this is where you would insert your policy
    observation, reward, terminated, truncated, info = env.step(action)
 
