819ed117c28cbf691633c41243d4dd1c9f81fdb8
SujayAmberkar <sujayamberkar@gmail.com>
2023-12-07 23:50:01 +0530

    integrated a2c algorithm in gym environment

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import gymnasium as gym
-env = gym.make("LunarLander-v2", render_mode="human")
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_util import make_vec_env
+
+env = gym.make("CartPole-v1", render_mode="human")
+model = A2C("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("a2c_cartpole")
+
+del model # remove to demonstrate saving and loading
+
+model = A2C.load("a2c_cartpole")
+
 observation, info = env.reset(seed=42)
+
 for _ in range(1000):
-   action = env.action_space.sample()  # this is where you would insert your policy
+   action,_states = model.predict(observation)  # this is where you would insert your policy
    observation, reward, terminated, truncated, info = env.step(action)
 
    if terminated or truncated:
